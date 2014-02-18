73d34010f1762befae6afc7b2e8a37be4a0c524e
Martti Malmi <martti.malmi@aalto.fi>
2014-02-18 23:02:08 +0200

    init

---
+++ bitcoin-otc.rb
@@ -0,0 +1,92 @@
+require 'rubygems'
+require 'nokogiri'
+require 'net/http'
+require 'json'
+require './identifi-rpc.rb'
+
+module BitcoinOTCCrawler
+  USER_LIST_FILE = "bitcoin-otc-data/bitcoin-otc-wot.html"
+  RATINGDETAILS_DIR = "bitcoin-otc-data/ratingdetails"
+  VIEWGPG_DIR = "bitcoin-otc-data/viewgpg"
+  LISTING_URI = URI("http://bitcoin-otc.com/viewratings.php")
+  IDENTIFI_HOST = 'http://identifirpc:FLg69hEBwf778rC4dSLEzdMtoyt41Ea8akffmGKHPLuf@127.0.0.1:8332'
+
+  def ratingdetailurl(nickname)
+    "http://bitcoin-otc.com/viewratingdetail.php?nick=#{nickname}&sign=ANY&type=RECV&outformat=json"
+  end
+
+  def viewgpgurl(nickname)
+    "http://bitcoin-otc.com/viewgpg.php?nick=#{nickname}&outformat=json"
+  end
+
+  def getuserlist
+    page = Nokogiri::HTML(open(USER_LIST_FILE))
+    userRows = page.css('table.datadisplay tr')
+    users = Array.new
+
+    userRows.drop(2).each do |row|
+      users.push row.css('td')[1].text
+    end
+
+    return users
+  end
+
+  def downloadRatingDetails(http)
+    request = Net::HTTP::Get.new ratingdetailurl(username)
+    response = http.request request # Net::HTTPResponse object
+
+    case response
+    when Net::HTTPSuccess then
+      out_file = File.new("#{RATINGDETAILS_DIR}/#{username}.json", "w")
+      out_file.puts(response.body)
+      out_file.close
+    else
+      puts response.value
+    end
+  end
+
+  def downloadViewGPG(http)
+    request = Net::HTTP::Get.new viewgpgurl(username)
+    response = http.request request # Net::HTTPResponse object
+
+    case response
+    when Net::HTTPSuccess then
+      out_file = File.new("#{VIEWGPG_DIR}/#{username}.json", "w")
+      out_file.puts(response.body)
+      out_file.close
+    else
+      puts response.value
+    end
+  end
+
+  def download
+  	Net::HTTP.start(LISTING_URI.host, LISTING_URI.port) do |http|
+      usernames = getuserlist
+
+      usernames.each_with_index do |username,i|
+        puts "[#{i} / #{usernames.size}] #{username}"
+        downloadRatingDetails(http)
+        downloadViewGPG(http)
+      end
+    end
+  end
+
+  def addToIdentifi
+  	identifi = IdentifiRPC.new(IDENTIFI_HOST)
+
+    Dir.foreach(RATINGDETAILS_DIR) do |item|
+      next if item == '.' or item == '..'
+      File.open( item, "r" ) do |f|
+        json = JSON.load( f )
+      end
+    end
+
+    Dir.foreach(VIEWGPG_DIR) do |item|
+      next if item == '.' or item == '..'
+      File.open( item, "r" ) do |f|
+        json = JSON.load( f )
+      end
+    end
+
+  end
+end
